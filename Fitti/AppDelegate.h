c8ba27f0c1cfaac4f093a02234a8ca7641d00eed
Ruben Green <ragreen@fb.com>
2020-07-13 17:12:55 -0700

    Initial Commit

---
+++ Fitti/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fitti
+//
+//  Created by Ruben Green on 7/13/20.
+//  Copyright © 2020 Ruben Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
