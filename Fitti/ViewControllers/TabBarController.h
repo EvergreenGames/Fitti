5b98eb2a1222808f182ace010888c8ffd11feeea
Ruben Green <ragreen@fb.com>
2020-07-14 13:42:59 -0700

    minor cleanup

---
+++ Fitti/ViewControllers/TabBarController.h
@@ -0,0 +1,17 @@
+//
+//  TabBarController.h
+//  Fitti
+//
+//  Created by Ruben Green on 7/13/20.
+//  Copyright © 2020 Ruben Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TabBarController : UITabBarController <UITabBarControllerDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Fitti/ViewControllers/TabBarController.m
@@ -0,0 +1,35 @@
+ //
+//  TabBarController.m
+//  Fitti
+//
+//  Created by Ruben Green on 7/13/20.
+//  Copyright © 2020 Ruben Green. All rights reserved.
+//
+
+#import "TabBarController.h"
+
+@interface TabBarController ()
+
+@property (nonatomic, strong) UIViewController* homeVC;
+@property (nonatomic, strong) UIViewController* postVC;
+
+@end
+
+@implementation TabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
