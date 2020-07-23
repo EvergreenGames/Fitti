cd39beeb8b2c00a7b3ef2acd76700c8ad75470a6
Ruben Green <ragreen@fb.com>
2020-07-23 16:12:53 -0700

    image viewing in details view

--- Fitti/ViewControllers/DetailsViewController.m
+++ Fitti/ViewControllers/DetailsViewController.m
@@ -7,12 +7,18 @@
 //
 
 #import "DetailsViewController.h"
+@import Parse;
 
 @interface DetailsViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *contentLabel;
 @property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet PFImageView *contentImageView;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *textHeightConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageHeightConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageAspectConstraint;
 
 @end
 
@@ -23,8 +29,23 @@
     
     if(self.post){
         self.titleLabel.text = self.post.title;
-        self.contentLabel.text = self.post.textContent;
         self.usernameLabel.text = self.post.author.username;
+        if([self.post.postType isEqualToString:@"text"]){
+            self.contentLabel.text = self.post.textContent;
+            self.textHeightConstraint.active = false;
+            self.imageHeightConstraint.active = true;
+        }
+        else if([self.post.postType isEqualToString:@"image"]){
+            self.textHeightConstraint.active=true;
+            self.imageAspectConstraint.constant = self.post.aspectRatio;
+            self.imageHeightConstraint.active=false;
+            self.contentImageView.file = self.post.image;
+            [self.contentImageView loadInBackground:^(UIImage * _Nullable image, NSError * _Nullable error) {
+                if(error){
+                    NSLog(@"Error loading image: %@", error.localizedDescription);
+                }
+            }];
+        }
     }
 }
 
